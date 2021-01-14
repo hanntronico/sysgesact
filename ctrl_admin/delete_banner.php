ae242fbc8f4b068f0f167e5245ce584bd1d40a8d
pitersoft <hanntronico@gmail.com>
2021-01-14 12:52:03 -0500

    se ha quitado los archivos que no son del sistema

--- ctrl_admin/delete_banner.php
+++
@@ -1,9 +0,0 @@
-<?php
-include ("conexion/config.php");
-$idse=$_GET["dat"];
-$rsh=("update silder set estado='E' where idsc='$idse'");
-$linkdocu->query($rsh);
-mysqli_close($linkdocu);
-   $mensaje="delete";
-header("Location: banner.php?var=$mensaje");
-?>
